a7a1ae245995301a466ff29be959e02ff7775516
David Carrero Real <dcarrea2302@g.educaand.es>
2025-09-30 13:49:20 +0200

    Primer commit

---
+++ suma.py
@@ -0,0 +1,5 @@
+# input lee como cadena y hace falta usar int para que lo lea como entero
+x = int(input("Primer numero"))
+y = int(input("Segundo numero"))
+z = x + y
+print("El resultado es", + z)
---
+++ usuario.py
@@ -0,0 +1,6 @@
+x = input("¿Cual es el usuario? ")
+y = x.capitalize()
+if y == ("Carreada"):
+ print("¡Bienvenido Carreada!")
+else:
+ print("Usuario incorrecto")
