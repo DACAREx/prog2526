7be30fea1c2f891d930097220d1394710bb65b7e
DACAREx <carrerorealdavid@gmail.com>
2025-10-01 12:15:17 +0200

    commit 2

---
+++ Nota.py
@@ -0,0 +1,14 @@
+nota = int(input("introduzca su nota"))
+match nota:
+  case 1 | 2 | 3 | 4 :
+       print(nota ," es un Insuficiente")
+  case 5:
+       print(nota ," es un Suficiente")
+  case 6:
+       print(nota ," es un Bien")
+  case 7 | 8 : 
+       print(nota ," es un Notable")
+  case 9 | 10:
+       print(nota ," es un sobresaliente")
+  case _:
+       print("a ocurrido un error")
