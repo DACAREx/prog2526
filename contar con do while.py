b1e9439be3b07200cbfb4ea3f445e55731571364
DACAREx <carrerorealdavid@gmail.com>
2025-10-07 09:41:48 +0200

    añadi error para < 0

--- contar con do while.py
+++ contar con do while.py
@@ -1,9 +1,12 @@
 n = int(input("Introduce el numero "))
 x = 0
-while True:
-   x = 1 + x
-   if x < n:
-       print(x, end=(", "))
-   else:
-       print(x) #asi salgo del bucle al ser mayor que el numero pedido y no pone coma al final
-       break 
+if n > 0:
+   while True:
+      x = 1 + x
+      if x < n:
+          print(x, end=(", "))
+      else:
+          print(x) #asi salgo del bucle al ser mayor que el numero pedido y no pone coma al final
+          break
+else:
+   print("Escribe un numero mayor que cero")
