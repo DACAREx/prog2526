8e5073a2123dea9411855c7c9b34b3fb29cb7d15
DACAREx <carrerorealdavid@gmail.com>
2025-10-30 10:26:12 +0100

    Delete tema1/scripts/par o impar funciones.py

--- tema1/scripts/par o impar funciones.py
+++
@@ -1,8 +0,0 @@
-def par_impar(num):
-    if num % 2 == 0:
-        resultado = print(f"El número {num} es par")
-    else:
-        resultado = print(f"El número {num} es impar")
-        
-num = int(input("Introduce un numero para saber si es par o impar "))
-par_impar(num)
