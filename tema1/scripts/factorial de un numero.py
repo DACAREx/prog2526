2ecf62bb25a5d7959fb46bdd6ff2a02701aa47e3
DACAREx <carrerorealdavid@gmail.com>
2025-10-30 10:10:18 +0100

    Delete tema1/scripts/factorial de un numero.py

--- tema1/scripts/factorial de un numero.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Introduce el numero al que quieras calcular su factorial "))
-fact = 1
-for i in range (n):
-    i = i + 1
-    fact = i * fact
-print("El factorial de",n, "es", fact)
- 
-    
-
