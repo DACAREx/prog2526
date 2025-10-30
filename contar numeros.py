373f2c4cfd1a1edb4b872c3a3f1da7165a7873cb
DACAREx <carrerorealdavid@gmail.com>
2025-10-30 10:29:41 +0100

    Delete contar numeros.py

--- contar numeros.py
+++
@@ -1,8 +0,0 @@
-N = int(input("Introduce un numero "))
-x = 0
-if N > 0:
-        while x < N:
-           x = x + 1
-           print(x, end=', ')
-else:
-    print("Ese numero no es válido(recuerda que tiene que ser positivo")
