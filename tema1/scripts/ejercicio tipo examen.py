3a96dbf978eb494ae1f4782e30411c0e2138e5d9
DACAREx <carrerorealdavid@gmail.com>
2025-10-30 10:10:08 +0100

    Delete tema1/scripts/ejercicio tipo examen.py

--- tema1/scripts/ejercicio tipo examen.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Introduce un numero mayor que 0: "))
-b = int(input("Introduce un numero mayor que 0: "))
-while b != 0:
-        if a>b:
-            a = a-b
-        else:
-            b = b-a
-print(a)
-    
