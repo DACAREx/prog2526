5d4df471adca9c645e28e69c4296b750db84a6f0
DACAREx <carrerorealdavid@gmail.com>
2025-10-30 10:32:38 +0100

    Delete suma.py

--- suma.py
+++
@@ -1,5 +0,0 @@
-# input lee como cadena y hace falta usar int para que lo lea como entero
-x = int(input("Primer numero"))
-y = int(input("Segundo numero"))
-z = x + y
-print("El resultado es", + z)
