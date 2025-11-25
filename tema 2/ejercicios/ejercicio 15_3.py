0c88a02837e381327ab070fd4f52a6abc51731f6
DACAREx <carrerorealdavid@gmail.com>
2025-11-25 13:54:52 +0100

    Delete tema 2/ejercicios directory

--- tema 2/ejercicios/ejercicio 15_3.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Introduce el numero del que desee saber su posicion: "))
-desorden = [3, 2, 5, 6, 9, 12, -8, 0, 21, 7]
-if num in desorden:
-    x = desorden.index(num)
-    print(x)
-else:
-    print(-1)
--- tema 2/ejercicios/ejercicio22.py
+++
@@ -1,8 +0,0 @@
-matriz = [["A","B","C"],["D","E","F"],["G","H","I"]]
-i = 0
-while i < len(matriz[0]):  
-    j = 0  
-    while j < len(matriz): 
-        print(matriz[j][i])
-        j = j + 1
-    i = i + 1
